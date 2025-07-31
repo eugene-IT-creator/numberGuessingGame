265ca88781b47dbe2f5574484a40e3b1a0761cac
Nielen Seldte <129792303+nielenseldte@users.noreply.github.com>
2025-07-31 11:23:16 +0200

    implemented getPlayerGuess method

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const generateRandomNumber = () => {
     return Math.floor(Math.random() * 100) + 1;
 }
 
+const getPlayerGuess = () => {
+    let guess;
+    let isValidInput = false;
+
+    while (!isValidInput) {
+        guess = Number(prompt("Please enter a number between 1 and 100:"));
+        if (guess == null) {
+            return null;
+        }
+        if (!isNaN(guess) && !(guess > 100) && !(guess < 1)) {
+            isValidInput = true;
+            break;
+        } else {
+            alert("Invalid Input");
+        }
+    }
+
+    return guess;
+}
